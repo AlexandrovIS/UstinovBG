8912c77eaf03c04b0aac8d566287cf2cdaa43aac
alexandrovis <Alexandrovis@gmail.com>
2021-07-07 18:05:54 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
-const bnt_menu=document.querySelector('.nav_icon')
+const btn_menu=document.querySelector('.nav_icon')
 const nav=document.querySelector('.nav')
 
 const clickHandler=()=>{
   nav.classList.toggle('nav_active')
-  bnt_menu.classList.toggle('nav_icon_active')
+  btn_menu.classList.toggle('nav_icon_active')
 }
 
-bnt_menu.addEventListener('click',clickHandler)
+btn_menu.addEventListener('click',clickHandler)
 
-const bnt_learn=document.querySelector('.arch__project-btn')
-if(bnt_learn){
-  bnt_learn.addEventListener('click',()=>{
+const btn_learn=document.querySelector('.arch__project-btn')
+if(btn_learn){
+  btn_learn.addEventListener('click',()=>{
   document.querySelector('.arch__project-article').classList.toggle('arch__project-article-active')
-  bnt_learn.classList.toggle('arch__project-btn-disable')
-
+  btn_learn.classList.toggle('arch__project-btn-disable')
 })
 }
+
+//Gallery
+const image_list=document.querySelectorAll('.photo-gallery-list img')
+const list_length=image_list.length
+let start_img=0
+const btn_slide_next=document.querySelector('.photo-gallery-list')
+
+const  checkImg=(index=start_img)=> {
+  image_list.forEach(item=>item.classList.remove('active_img'))
+  image_list[index].classList.add('active_img')
+}
+checkImg()
+
+btn_slide_next.addEventListener('click',()=>{
+ --start_img
+
+  if(start_img >= list_length){
+    start_img=0
+    checkImg(start_img)
+  } else if(start_img <= -1){
+    start_img=list_length-1
+    checkImg(start_img)
+  } else(
+    checkImg(start_img)
+  )
+})
 
 
