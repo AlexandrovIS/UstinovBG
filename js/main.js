a14c6ec85d0908ca226e8b08236698662de8068c
alexandrovis <Alexandrovis@gmail.com>
2021-07-15 13:37:06 +0300

    main branch slider

--- js/main.js
+++ js/main.js
@@ -19,12 +19,7 @@ if(btn_learn){
 //Gallery
 const image_list=document.querySelectorAll('.photo-gallery-list img')
 const list_length=image_list.length
-<<<<<<< HEAD
-let start_img=0
-let start_op=1
-=======
 let counter=0
->>>>>>> Slider
 
 const range_slide_next=document.querySelector('.photo-gallery-list .next__img-range')
 const range_slide_prev=document.querySelector('.photo-gallery-list .prev__img-range')
@@ -32,48 +27,12 @@ const btn_slide_prev=document.querySelector('.prev__img-btn')
 const btn_slide_next=document.querySelector('.next__img-btn')
 const alt_field=document.querySelector('.arch__photo-gallery-alt')
 
-<<<<<<< HEAD
-const  selectImg=(index=start_img)=> {
-=======
 const  selectImg=(index=0)=> {
->>>>>>> Slider
   image_list.forEach(item=>item.classList.remove('active_img'))
   image_list[index].classList.add('active_img')
   alt_field.innerHTML=image_list[index].alt
 }
 
-<<<<<<< HEAD
-const selectOpacityImg=(index=start_op)=>{
-  image_list[index].style.opacity=1;
-  console.log(index)
-}
-
-const checkIndex=num=>{
-  start_img+=num
-
-  if(start_img >= list_length){
-    start_img=0
-    selectImg(start_img)
-  } else if(start_img <= -1){
-    start_img=list_length-1
-    selectImg(start_img)
-  } else(
-    selectImg(start_img)
-  )
-}
-
-const checkIndexOpacity=(num)=>{
-  start_op+=num
-
-  if(start_op >= list_length){
-    start_op=0
-    selectOpacityImg(start_op)
-  } else if(start_op <= -1){
-    start_op=list_length-1
-    selectOpacityImg(start_op)
-  } else(
-    selectOpacityImg(start_op)
-=======
 const checkIndex=(index,func)=>{
   counter+=index
   
@@ -85,27 +44,15 @@ const checkIndex=(index,func)=>{
     func(counter)
   } else(
     func(counter)
->>>>>>> Slider
   )
 }
 
 if(image_list.length>0){
   selectImg()
-<<<<<<< HEAD
-  selectOpacityImg()
-  range_slide_prev.addEventListener('click',checkIndex.bind(this,-1))
-  btn_slide_prev.addEventListener('click',checkIndex.bind(this,-1))
-  range_slide_next.addEventListener('click',checkIndex.bind(this,1))
-  btn_slide_next.addEventListener('click',checkIndex.bind(this,1))
-
-  range_slide_next.addEventListener('click',checkIndexOpacity.bind(this,1))
-
-=======
   range_slide_prev.addEventListener('click',checkIndex.bind(this,-1,selectImg))
   btn_slide_prev.addEventListener('click',checkIndex.bind(this,-1,selectImg))
   range_slide_next.addEventListener('click',checkIndex.bind(this,1,selectImg))
   btn_slide_next.addEventListener('click',checkIndex.bind(this,1,selectImg))
->>>>>>> Slider
 }
 
 
